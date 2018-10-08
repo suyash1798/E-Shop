cfeda26055002f12ecc6e0f26670788f5156e171
suyash1798 <suyashtiwari1798@gmail.com>
2018-10-08 20:19:04 +0530

    more templates setup and urls

--- EcommerceShop/urls.py
+++ EcommerceShop/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    # path('shop/', include('shop.urls')),
+    path('shop/', include('shop.urls')),
     # path('search/', include('search_app.urls')),
     # path('cart/', include('cart.urls')),
     # path('order/', include('order.urls')),
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from shop.models import Product, Category
+
 
 def index(request):
     text_var = 'This is my'
     return HttpResponse(text_var)
+
+
+def allProdCat(request, c_slug=None):
+	c_page = None
+	products = None
+	if c_slug!=None:
+		c_page = get_object_or_404(Category,slug=c_slug)
+		products = Product.objects.filter(category=c_page,available=True)
+	else:
+		products_list = Product.objects.all().filter(available=True)
+	# '''Pagination code'''
+	# paginator = Paginator(products_list, 6)
+	# try:
+	# 	page = int(request.GET.get('page','1'))
+	# except:
+	# 	page = 1
+	# try:
+	# 	products = paginator.page(page)
+	# except (EmptyPage,InvalidPage):
+	# 	products = paginator.page(paginator.num_pages)
+	return render(request,'shop/category.html',{'category':c_page,'products':products})
